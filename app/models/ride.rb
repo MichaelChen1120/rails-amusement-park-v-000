5d7845f1603d4baef0403888f62dc10ce4bafff4
Michael Chen <michael.chen.1120@gmail.com>
2016-09-13 16:35:03 +0000

    Done.

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,3 +1,24 @@
 class Ride < ActiveRecord::Base
   # write associations here
+  belongs_to :user
+  belongs_to :attraction
+
+  def take_ride
+    user = User.find(self.user_id)
+    attraction = Attraction.find(self.attraction_id)
+
+    if user.tickets < attraction.tickets && user.height < attraction.min_height
+      message = "Sorry. You do not have enough tickets the #{attraction.name}. You are not tall enough to ride the #{attraction.name}."
+    elsif user.tickets < attraction.tickets
+      message = "Sorry. You do not have enough tickets the #{attraction.name}."
+    elsif user.height < attraction.min_height
+      message = "Sorry. You are not tall enough to ride the #{attraction.name}."
+    else
+      user.tickets = user.tickets - attraction.tickets
+      user.happiness = user.happiness + attraction.happiness_rating
+      user.nausea = user.nausea + attraction.nausea_rating
+      user.save
+      message = "Thanks for riding the #{self.attraction.name}!"
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+resources :users
+resources :attractions
 
+root to: 'sessions#welcome'
+
+get '/signin' => 'sessions#new'
+post '/signin' => 'sessions#create'
+get '/logout' => 'sessions#destroy'
+post '/logout' => 'sessions#destroy'
+post '/attractions/:id/go_on_ride' => 'attractions#go_on_ride'
 
 end
